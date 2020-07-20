a2ba7751ae2ae49e7efceab276e5c27d99fbda3f
Ravishankar Rajendran <rrajendran2@yahoo.com>
2020-07-20 13:03:17 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,33 +1,5 @@
-// document.getElementById('getjson').addEventListener('click', getJson);
+
 document.getElementById('getusers').addEventListener('click', getUsers);
-const counters =document.querySelectorAll('.counter');
-const food =document.getElementById('food');
-const transport =document.getElementById('transport');
-const home =document.getElementById('home');
-console.log(food.textContent);
-console.log(transport.textContent);
-console.log(home.textContent);
-
-
-// function getJson()
-// {
-//     // fetch ('json.txt')
-//     // .then(function(response){
-//     //     //console.log(response.text());
-//     //     //console.log(response);
-//     //     return response.text();
-//     // })
-//     // .then(function(data){
-//     //     console.log(data);
-//     // })
-
-//     fetch('json.txt')
-//     .then((response) => response.text())
-//     .then((data) => {
-//         document.getElementById('output').innerHTML = data
-//     })
-//     .catch((error) =>console.log (error))    
-// }
 
 function getUsers(){
     fetch('http://localhost:8080/carbonfootprint')
@@ -47,71 +19,8 @@ function getUsers(){
             `;
         });
         output = divRowOpen + output + divRowClose;
-        //document.getElementById('output').innerHTML = divRowOpen;
+        
         document.getElementById('output').innerHTML = output;
-        //document.getElementById('output').innerHTML = divRowClose;
+      
     })
 }
-// For Counter function
-// $(document).ready(function() {
-
-//     $('.counter').each(function () {
-//     $(this).prop('Counter',0).animate({
-//     Counter: $(this).text()
-//     }, {
-//     duration: 4000,
-//     easing: 'swing',
-//     step: function (now) {
-//     $(this).text(Math.ceil(now));
-//     }
-//     });
-//     });
-    
-//     });
-// For Counter function with text content
-// const counters =document.querySelectorAll('.counter');
-// const speed = 400;
-// counters.forEach(counter =>{
-//     const updateCount = () => {
-//         const target = counter.getAttribute('data-target');
-//         console.log(counter.textContent);        
-//         // get the value in span
-//         const count = +counter.innerText;
-//         //do the increment
-//         const inc= target/speed;
-        
-//         if(count<target) {
-//             counter.innerText = count++;
-//             setTimeout(updateCount,1);
-
-//         } else{
-//             count.innerText = target;
-//         }
-
-//     }
-//     updateCount();
-// });
-
-
-
-
-// counters.forEach(counter =>{
-//     const updateCount = () => {
-//         const target = counter.getAttribute('data-target');
-//         console.log(counter.textContent);        
-//         // get the value in span
-//         const count = +counter.innerText;
-//         //do the increment
-//         const inc= target/speed;
-        
-//         if(count<target) {
-//             counter.innerText = count+ inc;
-//             setTimeout(updateCount,1);
-
-//         } else{
-//             count.innerText = target;
-//         }
-
-//     }
-//     updateCount();
-// });
